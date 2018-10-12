8e462aaa66ab3b526d25878cd2ba8f3252e8314f
Unknown <jellesimonvds@gmail.com>
2018-10-12 10:56:44 +0200

    conveyor belts accesible voor alle 4 spelers gemaakt

--- Labyrinth party game/Assets/Developers/Scripts/ConveyorBelt.cs
+++ Labyrinth party game/Assets/Developers/Scripts/ConveyorBelt.cs
@@ -2,50 +2,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConveyorBelt : MonoBehaviour {
+public class ConveyorBelt : MonoBehaviour
+{
 
     [SerializeField]
-    private GameObject Player;
+    private GameObject[] Player;
 
     [SerializeField] private bool isRight;
     [SerializeField] private bool isLeft;
     [SerializeField] private bool isUp;
     [SerializeField] private bool isDown;
 
+
+    private int m_currentPlayer;
     Vector3 _Forward;
 
     private Vector3 _PlayerPos;
-    void Start ()
+    void Start()
     {
         _Forward = transform.forward;
         _Forward.y = 0;
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    }
+
+    void Update()
     {
-         _PlayerPos = Player.transform.position;	
-	}
+        _PlayerPos = Player[m_currentPlayer].transform.position;
+    }
     private void OnTriggerStay(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if (other.name == "Player 1")
         {
-            if(isUp == true)
-            {
-                Player.transform.Translate(Vector3.forward * 7.5f * Time.deltaTime, Space.World);
-            }
-            if (isDown == true)
-            {
-                Player.transform.Translate(Vector3.forward * -7.5f * Time.deltaTime, Space.World);
-            }
-            if (isRight == true)
-            {
-                Player.transform.Translate(Vector3.right * 7.5f * Time.deltaTime, Space.World);
-            }
-            if (isLeft == true)
-            {
-                Player.transform.Translate(Vector3.right * -7.5f * Time.deltaTime, Space.World);
-            }
+            SelectPlayer(0);
+        }
+        if (other.name == "Player 2")
+        {
+            SelectPlayer(1);
+        }
+        if (other.name == "Player 3")
+        {
+            SelectPlayer(2);
+        }
+        if (other.name == "Player 4")
+        {
+            SelectPlayer(3);
+        }
+    }
+    private void SelectPlayer(int _Player)
+    {
+        m_currentPlayer = _Player;
+        MovePlayer();
+    }
+    private void MovePlayer()
+    {
+        if(isUp == true)
+        {
+            Player[m_currentPlayer].transform.Translate(Vector3.forward * 7.5f * Time.deltaTime, Space.World);
+        }
+        if(isDown == true)
+        {
+            Player[m_currentPlayer].transform.Translate(Vector3.forward * -7.5f * Time.deltaTime, Space.World);
+        }
+        if(isRight == true)
+        {
+            Player[m_currentPlayer].transform.Translate(Vector3.right * 7.5f * Time.deltaTime, Space.World);
+        }
+        if(isLeft == true)
+        {
+            Player[m_currentPlayer].transform.Translate(Vector3.right * -7.5f * Time.deltaTime, Space.World);
         }
     }
 }
