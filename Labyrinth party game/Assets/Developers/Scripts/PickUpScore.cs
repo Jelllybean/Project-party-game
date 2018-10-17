06a89a6ef594d6a2416d301fda8b534f67140500
Unknown <jellesimonvds@gmail.com>
2018-10-17 12:43:51 +0200

    level design, points oprapen (oh yeah, merge conflict time)

--- Labyrinth party game/Assets/Developers/Scripts/PickUpScore.cs
+++ Labyrinth party game/Assets/Developers/Scripts/PickUpScore.cs
@@ -1,28 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickUpScore : MonoBehaviour {
 
-    private float[] _PlayerScore;
 
+    [SerializeField]
+    private Text[] m_PlayerScoreText;
+
+    private int[] _PlayerScore = new int[4];
+
+    private void Start()
+    {
+        m_PlayerScoreText[0].text = "Player 1: ";
+        m_PlayerScoreText[1].text = "Player 2: ";
+        m_PlayerScoreText[2].text = "Player 3: ";
+        m_PlayerScoreText[3].text = "Player 4: ";
+    }
+    private void Update()
+    {
+        print(_PlayerScore[0]);
+        print(_PlayerScore[1]);
+        print(_PlayerScore[2]);
+        print(_PlayerScore[3]);
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name == "Player 1")
+        if(gameObject.name == "Player 1")
         {
-
+            if (other.gameObject.tag == "Point")
+            {
+                _PlayerScore[0] += 1;
+                m_PlayerScoreText[0].text = "Player 1: " + _PlayerScore[0];
+                other.gameObject.SetActive(false);
+            }
         }
-        if (other.gameObject.name == "Player 2")
+        if (gameObject.name == "Player 2")
         {
-
+            if (other.gameObject.tag == "Point")
+            {
+                _PlayerScore[1] += 1;
+                m_PlayerScoreText[1].text = "Player 2: " + _PlayerScore[1];
+                other.gameObject.SetActive(false);
+            }
         }
-        if (other.gameObject.name == "Player 3")
+        if (gameObject.name == "Player 3")
         {
-
+            if (other.gameObject.tag == "Point")
+            {
+                _PlayerScore[2] += 1;
+                m_PlayerScoreText[2].text = "Player 3: " + _PlayerScore[2];
+                other.gameObject.SetActive(false);
+            }
         }
-        if (other.gameObject.name == "Player 4")
+        if (gameObject.name == "Player 4")
         {
-
+            if (other.gameObject.tag == "Point")
+            {
+                _PlayerScore[3] += 1;
+                m_PlayerScoreText[3].text = "Player 4: " + _PlayerScore[3];
+                other.gameObject.SetActive(false);
+            }
         }
+    }
+
+    public void AddScore(int m_Player)
+    {
+        _PlayerScore[m_Player] += 1;
     }
 }
---
+++ Labyrinth party game/Assets/Developers/Scripts/PlayerScoreText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScoreText : MonoBehaviour {
+
+    private int[] m_PlayerScore = new int[4];
+
+    [SerializeField]
+    private PickUpScore _PickUpScore;
+
+    private void Update()
+    {
+        //m_PlayerScore[0] = _PickUpScore._PlayerScore[0];
+        //m_PlayerScore[1] = _PickUpScore._PlayerScore[1];
+        //m_PlayerScore[2] = _PickUpScore._PlayerScore[2];
+        //m_PlayerScore[3] = _PickUpScore._PlayerScore[3];
+
+
+    }
+
+}
