65993960c401d0598488aea50af8f0d2ef458c44
Erik Nero Mounier <524102@student.glu.nl>
2018-11-13 17:59:40 +0100

    Added safezone system
    hoe je het script moet gebruiken staat in het script

---
+++ Labyrinth party game/Assets/Developers/Scripts/SafeZoneTimer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeZoneTimer : MonoBehaviour {
+    float time;
+    public float maxtime;
+	void Start () {
+		//zet dit script op een collider voor de safezone
+        // de collider moet staan op "Trigger"
+	}
+	void Update () {
+        time += Time.deltaTime;
+	}
+    private void OnTriggerStay(Collider other)
+    {
+        if(time >= maxtime)
+        {
+            if(other.name != "Char Player 1")
+            {
+                other.GetComponent<PickUpScore>().ResetScore(1);
+            }
+            if (other.name != "Char Player 2")
+            {
+                other.GetComponent<PickUpScore>().ResetScore(2);
+            }
+            if (other.name != "Char Player 2")
+            {
+                other.GetComponent<PickUpScore>().ResetScore(3);
+            }
+            if (other.name != "Char Player 2")
+            {
+                other.GetComponent<PickUpScore>().ResetScore(4);
+            }
+        }
+    }
+}
